9b4a512c641f5f45deac6667c64eb82f55e45ba5
Hesselek <hesselek@gmail.com>
2013-04-17 21:13:28 +0200

    Primer commit, con la estructura básica

---
+++ src/model/Main.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import model.Todo;
+
+public class Main {
+  private static final String PERSISTENCE_UNIT_NAME = "GDPA";
+  private static EntityManagerFactory factory;
+
+  public static void main(String[] args) {
+    factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    EntityManager em = factory.createEntityManager();
+    // Read the existing entries and write to console
+    Query q = em.createQuery("select t from Todo t");
+    List<Todo> todoList = q.getResultList();
+    for (Todo todo : todoList) {
+      System.out.println(todo);
+    }
+    System.out.println("Size: " + todoList.size());
+
+    // Create new todo
+    em.getTransaction().begin();
+    Todo todo = new Todo();
+    todo.setSummary("This is a test");
+    todo.setDescription("This is a test");
+    em.persist(todo);
+    em.getTransaction().commit();
+
+    em.close();
+
+    
+    
+    
+
+   
+  }
+} 
+
+
+
